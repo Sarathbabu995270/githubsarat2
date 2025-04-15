4311f0c11588ac495d91bccdac70b4cd5b281a22
Sarathbabu995270 <149604512+Sarathbabu995270@users.noreply.github.com>
2025-04-15 17:44:10 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,1 @@
-# Simple program to print numbers from 1 to 5 using a for loop
 
-for i in range(1, 6):  # range(1, 6) generates numbers from 1 to 5
-    print(f" {i}. Good Morning")
-# Simple program to print numbers from 1 to 5 using a while loop
-
-count = 1
-while count <= 5:  # Loop will run as long as count is less than or equal to 5
-    print("Number:", count)
-    count += 1  # Increment the count by 1
